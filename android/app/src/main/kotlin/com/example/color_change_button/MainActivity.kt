06201bf6e1f4303e7362cb8dc053bdd4c6260164
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-03-26 23:37:09 +0500

    my first app

---
+++ android/app/src/main/kotlin/com/example/color_change_button/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.color_change_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/colorchange.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class colorchange extends StatefulWidget {
+  const colorchange({super.key});
+
+  @override
+  State<colorchange> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<colorchange> {
+  bool ispress = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("COLOR CHANGE BUTTON APP"),
+        backgroundColor: Colors.blueAccent,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              ispress = !ispress;
+            });
+          },
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStatePropertyAll(ispress ? Colors.blue : Colors.red)),
+          child: const Text(
+            "PRESS HERE",
+            style:
+                TextStyle(color: Colors.black54, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
